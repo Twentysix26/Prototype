cc53e9072af2b0cae62c9450903086261a21c4ca
Twentysix <Twentysix26@users.noreply.github.com>
2020-08-26 23:28:01 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,16 +1,29 @@
 import yaml
+import sys
 
-with open("repositories.yaml") as f:
-    data = yaml.safe_load(f.read())
+if __name__ == "__main__":
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
 
-sh = ""
+    with open(infile) as f:
+        data = yaml.safe_load(f.read())
 
-repos = []
-repos.extend(data["approved"])
-repos.extend(data["unapproved"])
+    sh = ""
 
-for r in repos:
-    sh += f"git clone {r}\n"
+    repos = []
+    
+    if "approved" in repos:
+        repos.extend(data["approved"])
+    if "unapproved" in repos:
+        repos.extend(data["unapproved"])
 
-with open("cloneme.sh", "w") as f:
-    f.write(sh)
+    for r in repos:
+        if "@" in r:
+            repo, branch = r.split("@")
+            branch = branch.replace("/", "") # In case of leading /
+            sh += f"git clone {repo} --branch {branch} --single-branch\n"
+        else:
+            sh += f"git clone {r}\n"
+
+    with open(outfile, "w") as f:
+        f.write(sh)
